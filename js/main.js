092ca42881e75ca35d7f45301d6abdca4ce276b0
Sergey <s.nedosvitiy@gmail.com>
2023-11-21 15:28:37 +0200

    First task films in cycles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 let x = 15; 
-const result = confirm("Are you here?");
-console.log(result);
+PersonalMovieDB = {
+    movies: [],
+    privat : false
+};
+
+let fish = ["polo", undefined, "shark"];
+
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+for (let i = 0; i < 2; i++) {
+    const   a = prompt("What is your favorite film?", ""),
+            b = +prompt("What is it's rating?", "");
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50 && b !== NaN) { 
+        console.log('Done');
+        PersonalMovieDB.movies[a] = b;
+    }
+    else
+    {
+        i--;
+        console.log('Error');
+    }
+    
+    console.log(PersonalMovieDB);
+}
+
+    console.log(PersonalMovieDB.movies);
